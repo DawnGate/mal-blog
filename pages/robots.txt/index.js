51133af4e050e41aa983c33e1cef23f822f4400f
DawnGate <lam99quyen@gmail.com>
2023-09-17 01:21:53 +0700

    [dev] change robots.txt to dynamic, add empty favicon.ico file to public

---
+++ pages/robots.txt/index.js
@@ -0,0 +1,19 @@
+const baseUrl = process.env.NEXT_PUBLIC_HOST_URL ? process.env.NEXT_PUBLIC_HOST_URL : 'http://localhost:3000'
+
+function Robots() {
+  // Empty file
+}
+
+export async function getServerSideProps({ res }) {
+  res.setHeader('Content-Type', 'text/plain')
+  const robotsTxt = `User-agent: *\nAllow: /\nSitemap: ${baseUrl}/sitemap.xml\nHost: ${baseUrl}
+  `
+
+  res.write(robotsTxt)
+  res.end()
+  return {
+    props: {},
+  }
+}
+
+export default Robots
